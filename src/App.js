9b9b221296cb91fbd7544a2013d98dea8a9dc0d4
Nick Andreopoulos <nick_a96@hotmail.com>
2022-09-13 14:09:09 +0300

    verion-5

--- src/App.js
+++ src/App.js
@@ -4,8 +4,18 @@ import Card from "./components/Card";
 import Transactions from "./components/Transactions";
 //https://www.npmjs.com/package/react-tabs
 
+// Import Swiper React components
+//https://swiperjs.com/react
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Grid } from 'swiper';
+
+// Import Swiper styles
+import 'swiper/css';
+
 import {cardDatabase} from "./cardDatabaseDemo";
 import styles from "./styles/styles.css";
+
+
 
 function App() {
 
@@ -17,23 +27,47 @@ function App() {
 
       <div className="user-cards">
 
+        <Swiper
+          modules={[Grid]}
+          breakpoints={{
+            640: {
+              slidesPerView: 1,
+              spaceBetween: 20,
+            },
+            768: {
+              slidesPerView: 1,
+              spaceBetween: 40,
+            },
+            1024: {
+              slidesPerView: 3,
+              spaceBetween: 50,
+            },
+
+          }}
+          grid={{
+            rows:1
+          }}
+          className="cards-swiper"
+          
+          >
         {
 
             cardDatabase.map((card, key) => (
               
-              <Card 
-                key={key}
-                defaultClass={card.defaultClass}
-                type={card.type}
-                number={card.number}
-                holder={card.holder}
-                expiration={card.expiration}
-                color={card.color}
-
+              <SwiperSlide>
+                <Card
+                  key={key}
+                  defaultClass={card.defaultClass}
+                  type={card.type}
+                  number={card.number}
+                  holder={card.holder}
+                  expiration={card.expiration}
+                  color={card.color}
                 />
+              </SwiperSlide>
             ))
         }
-
+        </Swiper>
       </div>
 
       <div className="user-transactions">
